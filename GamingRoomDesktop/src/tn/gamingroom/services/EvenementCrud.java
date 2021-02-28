90ff02762044961aa85c2ace86ce88ae7b705375
farahayar <ayari.farah98@gmail.com>
2021-02-28 22:54:44 +0100

    Implementer la methode modifierEvenemment dans la classe EvenementCrud.java

--- GamingRoomDesktop/src/tn/gamingroom/services/EvenementCrud.java
+++ GamingRoomDesktop/src/tn/gamingroom/services/EvenementCrud.java
@@ -40,5 +40,28 @@ public class EvenementCrud implements IEvenement<Evenement>{
             System.out.println(ex.getMessage());
         }
     }
+
+    @Override
+    public void modifierEvenement(Evenement t) {
+        try {
+            String requete = "UPDATE personne SET nomevent=? datedeb=? datefin=? image=? categorie_id=? nbremax_participant=? description=? lieu=? lienyoutub WHERE id=?";
+            PreparedStatement pst = MyConnection.getInstance().getCnx()
+                    .prepareStatement(requete);
+            pst.setString(1, t.getNomEvent());
+            pst.setDate(2, t.getDateDeb());
+            pst.setDate(2, t.getDateFin());
+            pst.setString(2, t.getImage());
+            pst.setInt(2, t.getCategorie_id());
+            pst.setInt(2, t.getNbreMax_participant());
+            pst.setString(2, t.getDescription());
+            pst.setString(2, t.getLieu());
+            pst.setString(2, t.getLienYoutube());
+            pst.setInt(2, t.getIdevent());
+            pst.executeUpdate();
+            System.out.println("Personne modifiée");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
        
 }
