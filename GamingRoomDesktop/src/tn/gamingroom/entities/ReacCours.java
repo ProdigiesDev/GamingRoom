99b601bbf973c1c2bf121d84139b07d314df5f55
eya trifi <eya.trifi.19@gmail.com>
2021-03-02 00:29:25 +0100

    Add reaction cours

---
+++ GamingRoomDesktop/src/tn/gamingroom/entities/ReacCours.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.gamingroom.entities;
+
+/**
+ *
+ * @author eyatr
+ */
+public class ReacCours {
+    private int id;
+    private int nb_interaction;
+    private String commentaire;
+    private int membre_id ;
+    private int cour_id  ;
+
+    public ReacCours() {
+    }
+
+    public ReacCours(int id, int nb_interaction, String commentaire, int membre_id, int cour_id) {
+        this.id = id;
+        this.nb_interaction = nb_interaction;
+        this.commentaire = commentaire;
+        this.membre_id = membre_id;
+        this.cour_id = cour_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNb_interaction() {
+        return nb_interaction;
+    }
+
+    public void setNb_interaction(int nb_interaction) {
+        this.nb_interaction = nb_interaction;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public int getMembre_id() {
+        return membre_id;
+    }
+
+    public void setMembre_id(int membre_id) {
+        this.membre_id = membre_id;
+    }
+
+    public int getCour_id() {
+        return cour_id;
+    }
+
+    public void setCour_id(int cour_id) {
+        this.cour_id = cour_id;
+    }
+
+    @Override
+    public String toString() {
+        return "ReacCours{" + "id=" + id + ", nb_interaction=" + nb_interaction + ", commentaire=" + commentaire + ", membre_id=" + membre_id + ", cour_id=" + cour_id + '}';
+    }
+    
+    
+    
+}
