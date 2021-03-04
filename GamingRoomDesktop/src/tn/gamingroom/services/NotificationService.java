7d194c53ca8cc2bc4528e9b6a6179813017b7d31
farahayar <ayari.farah98@gmail.com>
2021-03-04 23:36:35 +0100

    Implementation de la methode ajoutNotification dans la classe NotificationService.java

--- GamingRoomDesktop/src/tn/gamingroom/services/NotificationService.java
+++ GamingRoomDesktop/src/tn/gamingroom/services/NotificationService.java
@@ -5,12 +5,35 @@
  */
 package tn.gamingroom.services;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import tn.gamingroom.entities.Notification;
 import tn.gamingroom.interfaces.INotification;
+import tn.gamingroom.outils.MyConnection;
 
 /**
  *
  * @author Asus
  */
-public class NotificationService implements INotification{
-    
+public class NotificationService implements INotification {
+
+    @Override
+    public void ajoutEvenement(Notification t) {
+        try {
+            String requete = "insert into notification (member_id,title,body) VALUES(?,?,?)";
+            PreparedStatement pst = MyConnection.getInstance().getCnx()
+                    .prepareStatement(requete);
+           
+            pst.setInt(1, t.getTo());
+            pst.setString(2, t.getTitle());
+            pst.setString(3, t.getBody());
+            pst.executeUpdate();
+            System.out.println("Notification inserée");
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
