2f86c834a5c52b8734685df80ecb7e02424254ba
farahayar <ayari.farah98@gmail.com>
2021-03-04 12:56:50 +0100

    Implementer lentite notification

---
+++ GamingRoomDesktop/src/tn/gamingroom/entities/notification.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.gamingroom.entities;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Asus
+ */
+public class notification {
+    public int id;
+    public int to;
+    public String title;
+    public String body;
+    public Date date;
+
+    public notification(int to, String title, String body, Date date) {
+        this.to = to;
+        this.title = title;
+        this.body = body;
+        this.date = date;
+    }
+
+    public notification(int id, int to, String title, String body, Date date) {
+        this.id = id;
+        this.to = to;
+        this.title = title;
+        this.body = body;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "notification{" + "id=" + id + ", to=" + to + ", title=" + title + ", body=" + body + ", date=" + date + '}';
+    }
+    
+    
+}
