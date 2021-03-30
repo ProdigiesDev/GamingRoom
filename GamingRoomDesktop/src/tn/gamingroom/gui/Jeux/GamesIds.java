7e51db994e2aa3e76f63e1c25a32eb956adc85a6
MohamedAmineDahwathi <mohamedamindahwathi@gmail.com>
2021-03-30 17:41:19 +0100

    changes in jeux service, add snake game , add tic toc toe game

---
+++ GamingRoomDesktop/src/tn/gamingroom/gui/Jeux/GamesIds.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.gamingroom.gui.Jeux;
+
+/**
+ *
+ * @author Dah
+ */
+public class GamesIds {
+    static final   int SNAKE_ID=14; 
+    static  final  int TIC_TAC_TOE=13;
+    
+}
