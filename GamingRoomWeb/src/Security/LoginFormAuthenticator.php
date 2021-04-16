4219645d90c1c9e68ddb6b9ab0733dbdd1009cc4
unknown <sonia.farhat@esprit.tn>
2021-04-16 08:19:28 +0100

    controle de saisie + modification au nv dashboard et accuil

--- GamingRoomWeb/src/Security/LoginFormAuthenticator.php
+++ GamingRoomWeb/src/Security/LoginFormAuthenticator.php
@@ -89,6 +89,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
+
     
         return new RedirectResponse($this->urlGenerator->generate('home'));
     }
