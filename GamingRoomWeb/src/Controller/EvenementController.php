0e6c1679868f137e0a3a58ba0780d6d001dcad73
farahayar <ayari.farah98@gmail.com>
2021-04-21 14:34:47 +0100

    creation du formu EvenementType.php

--- GamingRoomWeb/src/Controller/EvenementController.php
+++ GamingRoomWeb/src/Controller/EvenementController.php
@@ -2,19 +2,149 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use App\Form\EvenementType;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
+/**
+ * @Route("/evenement")
+ */
 class EvenementController extends AbstractController
 {
     /**
-     * @Route("/evenement", name="evenement")
+     * @Route("/", name="evenement_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
         return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
+            'evenements' => $evenementRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="evenement_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+                $file = $form->get('image')->getData();
+                $fileName = bin2hex(random_bytes(6)).'.'.$file->guessExtension();
+                $file->move ($this->getParameter('images_directory'),$fileName);
+                $evenement->setImage($fileName);
+                $evenement->setImage($fileName);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($evenement);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('evenement_index');
+            }
+
+            return $this->render('evenement/new.html.twig', [
+                'evenement' => $evenement,
+                'form' => $form->createView(),
+            ]);
+        }
+
+
+    /*public function new(Request $request, SluggerInterface $slugger): Response
+    {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $brochureFile = $form->get('brochure')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $evenement->setImage($newFilename);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($evenement);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('evenement_index');
+            }
+
+            return $this->render('evenement/new.html.twig', [
+                'evenement' => $evenement,
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+    */
+
+    /**
+     * @Route("/{idevent}", name="evenement_show", methods={"GET"})
+     */
+    public function show(Evenement $evenement): Response
+    {
+        return $this->render('evenement/show.html.twig', [
+            'evenement' => $evenement,
+        ]);
+    }
+
+    /**
+     * @Route("/{idevent}/edit", name="evenement_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Evenement $evenement): Response
+    {
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('evenement_index');
+        }
+
+        return $this->render('evenement/edit.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idevent}", name="evenement_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Evenement $evenement): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$evenement->getIdevent(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($evenement);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('evenement_index');
     }
 }
--- GamingRoomWeb/src/Entity/Categorie.php
+++ GamingRoomWeb/src/Entity/Categorie.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="categorie")
  * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\CategorieRepository")
+ *
  */
 class Categorie
 {
@@ -27,6 +29,35 @@ class Categorie
      * @ORM\Column(name="nomcategorie", type="string", length=30, nullable=false)
      */
     private $nomcategorie;
+
+    public function getIdcat(): ?int
+    {
+        return $this->idcat;
+    }
+
+    public function getNomcategorie(): ?string
+    {
+        return $this->nomcategorie;
+    }
+
+    public function setNomcategorie(string $nomcategorie): self
+    {
+        $this->nomcategorie = $nomcategorie;
+
+        return $this;
+    }
+
+    public function getCtegorie(): ?string
+    {
+        return $this->nomcategorie;
+    }
+
+    public function setCtegorie(string $nomcategorie): self
+    {
+        $this->nomcategorie = $nomcategorie;
+
+        return $this;
+    }
 
 
 }
