0e91e09c48d3b735cd92b5deed29ae99aca8bf34
unknown <sonia.farhat@esprit.tn>
2021-05-09 06:43:15 +0100

    ome changes in packages

--- GamingRoomMoblie/src/com/esprit/gamingroom/entities/Membre.java
+++ GamingRoomMoblie/src/com/esprit/gamingroom/entities/Membre.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tn.gamingroom.entities;
+
+package com.esprit.gamingroom.entities;
 
 import java.sql.Date;
 import java.time.LocalDate;
