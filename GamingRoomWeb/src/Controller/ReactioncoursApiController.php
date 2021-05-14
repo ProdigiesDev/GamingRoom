90c7c6b9998260560e6eca7ce41fa40393e6e238
Eya <eya.trifi.19@gmail.com>
2021-05-14 21:24:42 +0100

    fix untracked files

--- GamingRoomWeb/src/Controller/ReactioncoursApiController.php
+++ GamingRoomWeb/src/Controller/ReactioncoursApiController.php
@@ -17,4 +17,37 @@ class ReactioncoursApiController extends AbstractController
             'controller_name' => 'ReactioncoursApiController',
         ]);
     }
+
+
+    /**
+     * @Route("/addlikedeslike", name="likedeslike")
+     */
+    function addReactioncours($haveReactioncours, $typeReactioncours, $contenuCommentaire, $membre, $courId)
+    {
+        if ($haveReactioncours == null) {
+            $reactioncours = new Reactioncours();
+            $reactioncours->setInteraction($typeReactioncours);
+            $cour = $this->getDoctrine()->getManager()->getRepository(Cour::class)->find($courId);
+
+            $reactioncours->setCommentaire($contenuCommentaire);
+            $reactioncours->setCour($cour);
+            $reactioncours->setMembre($membre);
+
+            $date = new DateTime('now', new \DateTimeZone('Africa/Tunis'));
+            $reactioncours->setDateCreation($date);
+
+
+            $repository = $this->getDoctrine()->getManager();
+            $repository->persist($reactioncours);
+            $repository->flush();
+
+        } else {
+            foreach ($haveReactioncours as $haveReactioncour) {
+                $missionManager = $this->getDoctrine()->getManager();
+                $missionManager->remove($haveReactioncour);
+                $missionManager->flush();
+            }
+        }
+        return new Response($this->json(['code'=>201, 'message'=>'reaction added'],201));
+    }
 }
