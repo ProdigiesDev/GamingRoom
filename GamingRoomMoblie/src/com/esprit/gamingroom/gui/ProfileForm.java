25b90f711ac2b8501444b6061dfdff6579c68c11
unknown <sonia.farhat@esprit.tn>
2021-05-15 22:30:17 +0100

    add ProfileForm.java

---
+++ GamingRoomMoblie/src/com/esprit/gamingroom/gui/ProfileForm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.gamingroom.gui;
+
+import com.codename1.ui.Form;
+import com.codename1.ui.util.Resources;
+
+
+/**
+ *
+ * @author Sonia
+ */
+public class ProfileForm extends Form {
+    
+    public ProfileForm(Resources theme){
+        setTitle("Profile");
+    }
+}
