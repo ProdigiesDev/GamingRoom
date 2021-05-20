9646a8784465085c5d428ada4ea4c1e9330f1aeb
MohamedAmineDahwathi <mohamedamindahwathi@gmail.com>
2021-05-21 00:45:06 +0100

    after merge with sonia add constraints

--- GamingRoomMoblie/src/com/esprit/gamingroom/gui/SignUpForm.java
+++ GamingRoomMoblie/src/com/esprit/gamingroom/gui/SignUpForm.java
@@ -31,25 +31,28 @@ import com.esprit.gamingroom.services.MembreService;
 
 import com.codename1.ext.filechooser.FileChooser;
 import com.codename1.ui.Dialog;
+import com.codename1.ui.validation.LengthConstraint;
+import com.codename1.ui.validation.NumericConstraint;
+import com.codename1.ui.validation.RegexConstraint;
+import com.codename1.ui.validation.Validator;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-
-
-
-
 
 /**
  *
  * @author Sonia
  */
 public class SignUpForm extends Form {
+
     MembreService us;
     Membre m;
-        protected String saveFileToDevice(String hi, String ext) throws IOException {
+    Validator val = new Validator();
+
+    protected String saveFileToDevice(String hi, String ext) throws IOException {
         URI uri;
-           
+
         try {
             uri = new URI(hi);
             String path = uri.getPath();
@@ -60,7 +63,8 @@ public class SignUpForm extends Form {
         }
         return "hh";
     }
-    public SignUpForm(Resources res){
+
+    public SignUpForm(Resources res) {
         super(new BorderLayout(BorderLayout.CENTER_BEHAVIOR_CENTER_ABSOLUTE));
         this.us = MembreService.getInstance();
         setUIID("SignUpForm");
@@ -69,41 +73,43 @@ public class SignUpForm extends Form {
         Label titre = new Label();
         titre.setText("Sign Up");
         titre.setUIID("Title");
-        
+
         TextField nom = new TextField("", "Nom", 50, TextField.ANY);
         TextField prenom = new TextField("", "Prenom", 15, TextField.ANY);
-        Picker date=new Picker();
+        Picker date = new Picker();
         date.setType(Display.PICKER_TYPE_DATE);
-        
 
         ComboBox genre = new ComboBox();
         genre.addItem(Membre.Genre.Homme.toString());
         genre.addItem(Membre.Genre.Femme.toString());
-        
-        
+
         TextField Tel = new TextField("", "Telephone", 15, TextField.NUMERIC);
         TextField email = new TextField("", "Email", 50, TextField.EMAILADDR);
         TextField password = new TextField("", "Password", 15, TextField.PASSWORD);
-         TextField vpassword = new TextField("", "Password", 15, TextField.PASSWORD);
+        TextField vpassword = new TextField("", "Password", 15, TextField.PASSWORD);
         TextField image = new TextField("", "Image", 50, TextField.ANY);
 
-        
+        val.addConstraint(email, RegexConstraint.validEmail("Email doit etre valide"));
+        val.addConstraint(Tel, new RegexConstraint("^[0-9]{8}$","Telephone doit etre chaine des nombres"));
+        val.addConstraint(nom, new RegexConstraint("^[a-zA-Z]+$", "Nom doit etre chaine de caractère"));
+        val.addConstraint(prenom, new RegexConstraint("^[a-zA-Z]+$", "Prenom doit etre chaine de caractère"));
+
         ComboBox role = new ComboBox();
         role.addItem(Membre.Role.Membre.toString());
         role.addItem(Membre.Role.Coach.toString());
-        
+
         TextArea desc = new TextArea();
 //        desc.setRows(3);
 //        desc.setColumns(10);
 //        desc.setMaxSize(255);
         desc.setHint("Decrivez vous dans le domaine de Gaming ");
-         Button valider = new Button("Valider Inscription");
+        Button valider = new Button("Valider Inscription");
         valider.setUIID("LoginButton");
-               ///////////////image/////////////////////
+        ///////////////image/////////////////////
         Button img1 = new Button("Choisir une image");
         CheckBox multiSelect = new CheckBox();
-        img1.addActionListener((ActionEvent e)->{
-     if (FileChooser.isAvailable()) {
+        img1.addActionListener((ActionEvent e) -> {
+            if (FileChooser.isAvailable()) {
                 FileChooser.setOpenFilesInPlace(true);
                 FileChooser.showOpenDialog(multiSelect.isSelected(), ".jpg, .jpeg, .png/plain", (ActionEvent e2) -> {
                     if (e2 == null || e2.getSource() == null) {
@@ -129,7 +135,7 @@ public class SignUpForm extends Form {
 
                         try {
                             logo = Image.createImage(file).scaledHeight(500);;
-                          
+
                             String imageFile = FileSystemStorage.getInstance().getAppHomePath() + "photo.png";
 
                             try (OutputStream os = FileSystemStorage.getInstance().openOutputStream(imageFile)) {
@@ -154,41 +160,32 @@ public class SignUpForm extends Form {
                             try {
                                 String namePic = saveFileToDevice(file, ext);
                                 System.out.println(namePic);
-                                 valider.addActionListener((evt) -> {
-           
-                     MembreService.getInstance().signUp(nom,prenom,date.getDate(),genre.getSelectedItem().toString(),Tel,email,password,vpassword,namePic,role.getSelectedItem().toString(),desc);
+                                valider.addActionListener((evt) -> {
+                                    if (!val.isValid()) {
+                                      Dialog.show("Validation", "Verfier vous donner : 1-Telephone doit etre chaine des nombres 2-Nom et Prenom  doit etre chaine de caractère 3-Email doit etre valide", "OK", "");
+                                        return;
+                                    }
 
-                
-                try {
-               
-                
-               
-                    
-                    
-                } catch (NumberFormatException x) {
-                    Dialog.show("Alert", "nb par's status must be a number.",null, "OK");
-                }
-                
-                
-                
-            
-            
-            
-            
-        });
+                                    MembreService.getInstance().signUp(nom, prenom, date.getDate(), genre.getSelectedItem().toString(), Tel, email, password, vpassword, namePic, role.getSelectedItem().toString(), desc);
+
+                                    try {
+
+                                    } catch (NumberFormatException x) {
+                                        Dialog.show("Alert", "nb par's status must be a number.", null, "OK");
+                                    }
+
+                                });
                             } catch (IOException ex) {
                             }
 
                             revalidate();
-                            
 
-                        
+                        }
                     }
-                    }
-                        });
+                });
             }
         });
-        
+
 //        Button valider = new Button("Valider Inscription");
 //        valider.setUIID("LoginButton");
 //        valider.addActionListener(e -> {
@@ -196,28 +193,24 @@ public class SignUpForm extends Form {
 //
 //
 //       });
-        
-        
         Button retour = new Button("<< Retour SignIn");
         retour.setUIID("CreateNewAccountButton");
         retour.addActionListener(e -> new SignInForm(res).show());
 
-        
 //         nom.getAllStyles().setMargin(LEFT, 0);
 //         prenom.getAllStyles().setMargin(LEFT, 0);
 //         date.getAllStyles().setMargin(LEFT, 0);
 //         genre.getAllStyles().setMargin(LEFT, 0);
-        
-         Container by;
+        Container by;
         by = BoxLayout.encloseY(
-//                spaceLabel,
+                //                spaceLabel,
                 titre,
+                img1,
                 nom,
                 prenom,
                 date,
                 genre,
                 Tel,
-                img1,
                 email,
                 password,
                 vpassword,
@@ -225,10 +218,9 @@ public class SignUpForm extends Form {
                 desc,
                 valider,
                 retour
-               
         );
-       
+
         add(BorderLayout.NORTH, by);
-        
+
     }
 }
